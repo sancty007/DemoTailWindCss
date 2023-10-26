76cc70350579b94cb984ecfd3991b43139a04afe
sancty <sanctifieryawmensah55@gmail.com>
2023-10-26 14:18:33 +0100

    ajout des listes à puces

--- fonction.js
+++ fonction.js
@@ -7,8 +7,6 @@ const list = document.querySelector(".myList")
 // parcours du tableau avec forEach 
 myList.forEach(element => {
 const listItem = document.createElement("li") // creation du liste 
-listItem.textContent = element // Ajout des elemenst du tableau dans liste 
-
-listItem.classList.add("text-xl") // Modification de la taille des disc 
-list.appendChild(listItem) // liaision avec la class .myList 
+listItem.textContent = element // Ajout des elemenst du tableau dans liste  
+list.appendChild(listItem) // liaision avec la class parente ".myList" 
 });
