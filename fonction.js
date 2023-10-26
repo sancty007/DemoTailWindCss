bd2ae3e3ea46efcfb323661d6cefbe55ba59c472
sancty <sanctifieryawmensah55@gmail.com>
2023-10-26 13:34:48 +0100

    ajout commentaire dans le fichier js

--- fonction.js
+++ fonction.js
@@ -1,8 +1,14 @@
 
+// creation du tableau , contient des données 
 const myList = ["2 каюты", "4 спальных места", "аудиосистема, кондиционер", "холодильник, микроволновка", "душ, туалет"]
+
 const list = document.querySelector(".myList")
+
+// parcours du tableau avec forEach 
 myList.forEach(element => {
-const listItem = document.createElement("li")
-listItem.textContent = element
-list.appendChild(listItem)
+const listItem = document.createElement("li") // creation du liste 
+listItem.textContent = element // Ajout des elemenst du tableau dans liste 
+
+listItem.classList.add("text-xl") // Modification de la taille des disc 
+list.appendChild(listItem) // liaision avec la class .myList 
 });
