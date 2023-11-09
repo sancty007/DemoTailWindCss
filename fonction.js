1c3490f63ba985da79fba0e6f5c4e5e6d36ba5fd
brx-nbif <blondeau.nbif@mail.ru>
2023-11-09 17:35:11 +0300

    Voici ma petite review sur ton code, je suis un peu trop occupé ces jours-ci cher ami

--- fonction.js
+++ fonction.js
@@ -1,12 +1,17 @@
+// creation du tableau , contient des données
+const myList = [
+  "2 каюты",
+  "4 спальных места",
+  "аудиосистема, кондиционер",
+  "холодильник, микроволновка",
+  "душ, туалет",
+];
 
-// creation du tableau , contient des données 
-const myList = ["2 каюты", "4 спальных места", "аудиосистема, кондиционер", "холодильник, микроволновка", "душ, туалет"]
+const list = document.querySelector("[data-list]");
 
-const list = document.querySelector(".myList")
-
-// parcours du tableau avec forEach 
-myList.forEach(element => {
-const listItem = document.createElement("li") // creation du liste 
-listItem.textContent = element // Ajout des elemenst du tableau dans liste  
-list.appendChild(listItem) // liaision avec la class parente ".myList" 
+// parcours du tableau avec forEach
+myList.forEach((element) => {
+  const listItem = document.createElement("li"); // creation du liste
+  listItem.textContent = element; // Ajout des elemenst du tableau dans liste
+  list.appendChild(listItem); // liaision avec la class parente ".myList"
 });
